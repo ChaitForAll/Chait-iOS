202e86a9a54634c0b9e0a3a41852aa87641a6c5b
yjjem <marksim0606@gmail.com>
2025-01-13 03:10:34 +0900

    Add Chait XcodeProject

---
+++ Sources/Chait/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
