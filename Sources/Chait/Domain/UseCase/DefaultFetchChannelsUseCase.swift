c9dd1d9996aabf8593a960316837f00039bf4d61
yjjem <marksim0606@gmail.com>
2025-01-14 17:33:56 +0900

    feat: implement DefaultFetchChannelsUseCase

---
+++ Sources/Chait/Domain/UseCase/DefaultFetchChannelsUseCase.swift
@@ -0,0 +1,32 @@
+//
+//  DefaultFetchChannelsUseCase.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Combine
+
+final class DefaultFetchChannelsUseCase: FetchChannelsUseCase {
+    
+    // MARK: Property(s)
+    
+    private let channelsRepository: ChannelRepository
+    
+    init(channelsRepository: ChannelRepository) {
+        self.channelsRepository = channelsRepository
+    }
+    
+    // MARK: Function(s)
+    
+    func fetchAllChannels() -> AnyPublisher<[Channel], FetchChannelsError> {
+        channelsRepository.fetchAllChannels()
+            .mapError { error in
+                switch error {
+                case .noAvailableChannels: return .emptyChannelList
+                case .unknown: return FetchChannelsError.unknown
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+}
