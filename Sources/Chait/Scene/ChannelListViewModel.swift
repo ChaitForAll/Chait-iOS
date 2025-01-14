636ce3848442f22eddc2292643037dcf237b9c78
yjjem <marksim0606@gmail.com>
2025-01-14 19:00:44 +0900

    feat(ChannelListViewModel): add channels preparation

--- Sources/Chait/Scene/ChannelListViewModel.swift
+++ Sources/Chait/Scene/ChannelListViewModel.swift
@@ -12,9 +12,29 @@ final class ChannelListViewModel {
     
     let fetchedChannels: PassthroughSubject<[ChannelPresentationModel.ID], Never> = .init()
     
+    private var channels: [ChannelPresentationModel.ID : ChannelPresentationModel] = [:]
+    private var cancelBag: Set<AnyCancellable> = .init()
+    
     private let fetchChannelsUseCase: FetchChannelsUseCase
     
     init(fetchChannelsUseCase: FetchChannelsUseCase) {
         self.fetchChannelsUseCase = fetchChannelsUseCase
+    }
+    
+    func prepareChannels() {
+        fetchChannelsUseCase.fetchAllChannels()
+            .map { channels -> [ChannelPresentationModel] in
+                return channels.map { channel in
+                    return ChannelPresentationModel(id: channel.id, title: channel.title)
+                }
+            }
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { receivedChannels in
+                    receivedChannels.forEach { self.channels[$0.id] = $0 }
+                    self.fetchedChannels.send(receivedChannels.map { $0.id })
+                }
+            )
+            .store(in: &cancelBag)
     }
 }
