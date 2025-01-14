c65cf7824b8e1b3a8b929527f82bd78a2d11fbcb
yjjem <marksim0606@gmail.com>
2025-01-14 19:51:15 +0900

    feat: add ViewModel binding and list cell configuration

--- Sources/Chait/Scene/ChannelListViewController.swift
+++ Sources/Chait/Scene/ChannelListViewController.swift
@@ -3,9 +3,10 @@
 //  Chait
 //
 //  Copyright (c) 2025 Jeremy All rights reserved.
-    
+
 
 import UIKit
+import Combine
 
 final class ChannelListViewController: UIViewController {
     
@@ -16,23 +17,45 @@ final class ChannelListViewController: UIViewController {
     }
     
     typealias DiffableDataSource = UICollectionViewDiffableDataSource<Section, ChannelPresentationModel.ID>
+    typealias ListCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, ChannelPresentationModel.ID>
     
     // MARK: Property(s)
     
+    var viewModel: ChannelListViewModel?
+    
     private var diffableDataSource: DiffableDataSource?
+    private var cancelBag: Set<AnyCancellable> = .init()
     
     private let collectionView: UICollectionView = .init(
         frame: .zero, collectionViewLayout: .init()
     )
     
     // MARK: Override(s)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        bindViewModel()
+        viewModel?.prepareChannels()
+    }
+    
+    // MARK: Private Function(s)
+    
+    private func bindViewModel() {
+        viewModel?.fetchedChannels
+            .sink {
+                var snapShot = NSDiffableDataSourceSnapshot<Section, ChannelPresentationModel.ID>()
+                snapShot.appendSections([.channelList])
+                snapShot.appendItems($0)
+                self.diffableDataSource?.apply(snapShot)
+            }
+            .store(in: &cancelBag)
+    }
+    
+    private func configureCollectionView() {
         view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.setCollectionViewLayout(createListLayout(), animated: false)
-        collectionView.backgroundColor = .systemGreen
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -42,13 +65,28 @@ final class ChannelListViewController: UIViewController {
         self.diffableDataSource = createDataSource()
     }
     
-    // MARK: Private Function(s)
+    private func createListCellRegistration() -> ListCellRegistration {
+        return ListCellRegistration { [weak self] cell, indexPath, itemIdentifier in
+            
+            var cellContent = cell.defaultContentConfiguration()
+            if let channelModel = self?.viewModel?.channel(for: itemIdentifier) {
+                cellContent.text = channelModel.title
+                cellContent.image = UIImage(systemName: "rectangle.fill")
+            }
+            cell.contentConfiguration = cellContent
+        }
+    }
     
     private func createDataSource() -> DiffableDataSource {
-        let dataSource = DiffableDataSource(collectionView: collectionView) { 
+        let cellRegistration = createListCellRegistration()
+        let dataSource = DiffableDataSource(collectionView: collectionView) {
             collectionView, indexPath, itemIdentifier in
             
-            return UICollectionViewCell()
+            collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
         }
         return dataSource
     }
