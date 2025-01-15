bc3661270d2aab5140df336cf3daa93d1fdfda8e
yjjem <marksim0606@gmail.com>
2025-01-15 21:15:25 +0900

    feat: implement DefaultCreateNewChannelUseCase

---
+++ Sources/Chait/Domain/UseCase/DefaultCreateNewChannelUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultCreateNewChannelUseCase.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Combine
+
+final class DefaultCreateNewChannelUseCase: CreateNewChannelUseCase {
+    
+    // MARK: Property(s)
+    
+    private let repository: ChannelRepository
+    
+    init(repository: ChannelRepository) {
+        self.repository = repository
+    }
+    
+    // MARK: Function(s)
+    
+    func create(newChannel: NewChannel) -> AnyPublisher<Void, CreateNewChannelError> {
+        guard newChannel.isValid() else {
+            return Result<Void, CreateNewChannelError>
+                .Publisher(.failure(.invalidInput("Please check channel specifications.")))
+                .eraseToAnyPublisher()
+        }
+        
+        return repository
+            .createNewChannel(newChannel)
+            .mapError { _ in  return CreateNewChannelError.unknown }
+            .eraseToAnyPublisher()
+    }
+}
