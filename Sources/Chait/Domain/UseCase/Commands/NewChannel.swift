5cae0b20fa4e55d4466c8ca876e1264f98136ba7
yjjem <marksim0606@gmail.com>
2025-01-15 21:10:28 +0900

    refactor(NewChannel): add validation function

--- Sources/Chait/Domain/UseCase/Commands/NewChannel.swift
+++ Sources/Chait/Domain/UseCase/Commands/NewChannel.swift
@@ -9,4 +9,12 @@ import Foundation
 
 struct NewChannel {
     let title: String
+    
+    func isValid() -> Bool {
+        let validations: [Bool] = [
+            !title.isEmpty,
+            title != ""
+        ]
+        return validations.allSatisfy({ $0 == true })
+    }
 }
