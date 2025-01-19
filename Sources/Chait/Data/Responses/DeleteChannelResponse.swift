25392718f0cf221dfb480e91f011089bc7f8d732
yjjem <marksim0606@gmail.com>
2025-01-19 15:38:45 +0900

    feat: add DeleteChannelResponse

---
+++ Sources/Chait/Data/Responses/DeleteChannelResponse.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteChannelResponse.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct DeleteChannelResponse: Decodable {
+    let id: UUID
+}
