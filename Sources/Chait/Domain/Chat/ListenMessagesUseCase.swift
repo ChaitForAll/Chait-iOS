575ae087b87bc784cc38d0ecd6e67712aa640749
yjjem <marksim0606@gmail.com>
2025-02-25 23:32:43 +0900

    feat(Domain): add ListenMessagesUseCase

--- Sources/Chait/Domain/Chat/ListenMessagesUseCase.swift
+++ Sources/Chait/Domain/Chat/ListenMessagesUseCase.swift
@@ -11,3 +11,18 @@ import Combine
 protocol ListenMessagesUseCase {
     func startListening(channelID: UUID) -> AnyPublisher<[Message], Never>
 }
+
+final class DefaultListenMessagesUseCase: ListenMessagesUseCase {
+    
+    private let chatRepository: ChatRepository
+    
+    init(chatRepository: ChatRepository) {
+        self.chatRepository = chatRepository
+    }
+    
+    func startListening(channelID: UUID) -> AnyPublisher<[Message], Never> {
+        chatRepository
+            .startListeningMessages(channelID: channelID)
+            .eraseToAnyPublisher()
+    }
+}
