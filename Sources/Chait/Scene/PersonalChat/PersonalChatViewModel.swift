ab8499ad7d839c3b86dc37abce8fa1a39e5f8374
yjjem <marksim0606@gmail.com>
2025-02-26 23:09:00 +0900

    feat(Domain): Add SendMessageUseCase and onSendMessage()

--- Sources/Chait/Scene/PersonalChat/PersonalChatViewModel.swift
+++ Sources/Chait/Scene/PersonalChat/PersonalChatViewModel.swift
@@ -3,7 +3,7 @@
 //  Chait
 //
 //  Copyright (c) 2025 Jeremy All rights reserved.
-    
+
 
 import Foundation
 import Combine
@@ -13,10 +13,22 @@ final class PersonalChatViewModel {
     var userMessageText: String = ""
     
     private var cancelBag: Set<AnyCancellable> = .init()
-
-    private let channelID: UUID
     
-    init(channelID: UUID) {
+    private let channelID: UUID
+    private let sendMessageUseCase: SendMessageUseCase
+    
+    init(channelID: UUID, sendMessageUseCase: SendMessageUseCase) {
         self.channelID = channelID
+        self.sendMessageUseCase = sendMessageUseCase
+    }
+    
+    func onSendMessage() {
+        sendMessageUseCase
+            .sendMessage(text: userMessageText, senderID: UUID(), channelID: channelID)
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { _ in }
+            )
+            .store(in: &cancelBag)
     }
 }
