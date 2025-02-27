ca8e6698b028a059fbb04fc6285324fb79902da2
yjjem <marksim0606@gmail.com>
2025-02-27 15:43:38 +0900

    feat(Scene): Integrate MessageListening to PersonalChatViewModel

--- Sources/Chait/Scene/PersonalChat/PersonalChatViewModel.swift
+++ Sources/Chait/Scene/PersonalChat/PersonalChatViewModel.swift
@@ -10,17 +10,28 @@ import Combine
 
 final class PersonalChatViewModel {
     
+    // MARK: Property(s)
+    
+    @Published var messages: [String] = []
     var userMessageText: String = ""
     
     private var cancelBag: Set<AnyCancellable> = .init()
     
     private let channelID: UUID
     private let sendMessageUseCase: SendMessageUseCase
+    private let listenMessagesUseCase: ListenMessagesUseCase
     
-    init(channelID: UUID, sendMessageUseCase: SendMessageUseCase) {
+    init(
+        channelID: UUID,
+        sendMessageUseCase: SendMessageUseCase, 
+        listenMessagesUseCase: ListenMessagesUseCase
+    ) {
         self.channelID = channelID
         self.sendMessageUseCase = sendMessageUseCase
+        self.listenMessagesUseCase = listenMessagesUseCase
     }
+    
+    // MARK: Function(s)
     
     func onSendMessage() {
         sendMessageUseCase
@@ -28,6 +39,20 @@ final class PersonalChatViewModel {
             .sink(
                 receiveCompletion: { _ in },
                 receiveValue: { _ in }
+            )
+            .store(in: &cancelBag)
+    }
+    
+    func startListening() {
+        listenMessagesUseCase
+            .startListening(channelID: channelID)
+            .map { messages in
+                messages.map { $0.text } }
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { [weak self ] receivedMessages in
+                    self?.messages.append(contentsOf: receivedMessages)
+                }
             )
             .store(in: &cancelBag)
     }
