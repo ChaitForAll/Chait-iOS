d0d192af005cff67a5a449faed5aae2c3ee041d2
yjjem <marksim0606@gmail.com>
2025-02-28 00:16:25 +0900

    test(Scene): Add test_UpdatesMessagesOnListenerReceiveMessages

--- Sources/ChaitTests/Unit/Scene/PersonalChatViewModelTests.swift
+++ Sources/ChaitTests/Unit/Scene/PersonalChatViewModelTests.swift
@@ -6,9 +6,12 @@
     
 @testable import Chait
 import Foundation
+import Combine
 import XCTest
 
 final class PersonalChatViewModelTests: XCTestCase {
+    
+    private var cancelBag: Set<AnyCancellable> = .init()
     
     // MARK: Test(s)
     
@@ -33,5 +36,56 @@ final class PersonalChatViewModelTests: XCTestCase {
         // Assert
         
         XCTAssertTrue(sut.userMessageText.isEmpty)
+    }
+    
+    func test_UpdatesMessagesOnListenerReceiveMessages() {
+        
+        // Arrange
+        
+        let fakeChannelID = UUID()
+        let fakeSendMessagesUseCase = StubSendMessageSucceed()
+        
+        let expectedMessages = (0..<5).map {
+            return Message(
+                text: "Test Message \($0)",
+                messageID: UUID(),
+                senderID: UUID(),
+                channelID: UUID(),
+                createdAt: Date.now
+            )
+        }
+        
+        let sut = PersonalChatViewModel(
+            channelID: fakeChannelID,
+            sendMessageUseCase: fakeSendMessagesUseCase,
+            listenMessagesUseCase: StubListenMessageReceiveMessages(
+                messages: expectedMessages
+            )
+        )
+        
+        let expectation = XCTestExpectation(description: "receives messages 5 times")
+        expectation.expectedFulfillmentCount = 5
+        
+        // Act
+        
+        var receivedMessages: [String] = []
+        
+        sut.startListening()
+        
+        sut.messages.publisher
+            .sink { message in
+                receivedMessages.append(message)
+                expectation.fulfill()
+                print(message)
+            }
+            .store(in: &cancelBag)
+        
+        // Assert
+        
+        wait(for: [expectation], timeout: 1.0)
+        
+        for (received, expected) in zip(expectedMessages, receivedMessages) {
+            XCTAssertEqual(received.text, expected)
+        }
     }
 }
