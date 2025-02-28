be22a10f3336751235bd3012b1d0227055d2f836
yjjem <marksim0606@gmail.com>
2025-02-28 23:46:07 +0900

    refactor(Data): Add CodingKeys to MessageResponse

--- Sources/Chait/Data/RemoteMessages/MessageResponse.swift
+++ Sources/Chait/Data/RemoteMessages/MessageResponse.swift
@@ -13,4 +13,12 @@ struct MessageResponse: Decodable {
     let senderID: UUID
     let channelID: UUID
     let createdAt: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case text
+        case messageID = "id"
+        case senderID = "sender"
+        case channelID = "channel_id"
+        case createdAt = "created_at"
+    }
 }
