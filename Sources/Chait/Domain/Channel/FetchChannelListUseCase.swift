f2fe606a6cd80959e2239c698d72f12aee294c83
yjjem <marksim0606@gmail.com>
2025-03-02 23:02:21 +0900

    feat(Channel): Add FetchChannelListUseCase Implementation

--- Sources/Chait/Domain/Channel/FetchChannelListUseCase.swift
+++ Sources/Chait/Domain/Channel/FetchChannelListUseCase.swift
@@ -9,9 +9,28 @@ import Foundation
 import Combine
 
 enum FetchChannelListUseCaseError: Error {
-    case unknown(_ error: Error)
+    case noChannels
+    case networkError
+    case fetchFailed
 }
 
 protocol FetchChannelListUseCase {
     func fetchChannels(_ userID: UUID) -> AnyPublisher<[Channel], FetchChannelListUseCaseError>
+}
+
+final class DefaultFetchChannelListUseCase: FetchChannelListUseCase {
+    
+    // MARK: Property(s)
+    
+    private let repository: ChannelRepository
+    
+    init(repository: ChannelRepository) {
+        self.repository = repository
+    }
+    
+    // MARK: Function(s)
+    
+    func fetchChannels(_ userID: UUID) -> AnyPublisher<[Channel], FetchChannelListUseCaseError> {
+        return repository.fetchChannels(userID: userID)
+    }
 }
