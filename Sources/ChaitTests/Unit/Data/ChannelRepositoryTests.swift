3823c12612b407f1a3277ecc57972c665bb86940
yjjem <marksim0606@gmail.com>
2025-03-03 23:18:28 +0900

    test(Data): Add ChannelRepositoryTests

---
+++ Sources/ChaitTests/Unit/Data/ChannelRepositoryTests.swift
@@ -0,0 +1,15 @@
+//
+//  ChannelRepositoryTests.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+
+
+@testable import Chait
+import Foundation
+import Combine
+import XCTest
+
+final class ChannelRepositoryTests: XCTestCase {
+    
+}
