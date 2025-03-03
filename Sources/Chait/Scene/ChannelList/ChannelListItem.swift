69eb3f7a908219f5d17be9213caf89ac92755f4a
yjjem <marksim0606@gmail.com>
2025-03-03 23:50:30 +0900

    feat(ChannelList): Add ChannelListItem

---
+++ Sources/Chait/Scene/ChannelList/ChannelListItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChannelListItem.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct ChannelListItem: Identifiable {
+    let id: UUID
+    let title: String
+}
