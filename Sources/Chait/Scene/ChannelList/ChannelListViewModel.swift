8a057e8bfdb20fcc0f5953bf2d495fcef375969c
yjjem <marksim0606@gmail.com>
2025-03-03 23:56:42 +0900

    feat(ChannelList): Add ChannelListViewModel

--- Sources/Chait/Scene/ChannelList/ChannelListViewModel.swift
+++ Sources/Chait/Scene/ChannelList/ChannelListViewModel.swift
@@ -10,6 +10,18 @@ import Combine
 
 final class ChannelListViewModel {
     
+    struct Output {
+        let fetchedChannelListItems: AnyPublisher<[UUID], Never>
+    }
+    
+    // MARK: Property(s)
+    
+    private var itemsInOrder: [ChannelListItem] = []
+    private var itemsMap: [ChannelListItem.ID: ChannelListItem] = [:]
+    private var cancelBag: Set<AnyCancellable> = .init()
+    
+    private let fetchedChannelListItems: PassthroughSubject<[UUID], Never> = .init()
+    
     private let fetchChannelListUseCase: FetchChannelListUseCase
     private let userID: UUID
     
@@ -17,4 +29,30 @@ final class ChannelListViewModel {
         self.fetchChannelListUseCase = fetchChannelListUseCase
         self.userID = userID
     }
+    
+    // MARK: Function(s)
+    
+    func bindOutput() -> Output {
+        return Output(fetchedChannelListItems: fetchedChannelListItems.eraseToAnyPublisher())
+    }
+    
+    func onNeedItems() -> AnyPublisher<[UUID], Never> {
+        fetchChannelListUseCase.fetchChannels(userID)
+            .map { channels in
+                channels.map {
+                    ChannelListItem(id: $0.channelID, title: $0.title)
+                }
+            }
+            .sink { _ in
+                // TODO: Add Error handling flow
+            } receiveValue: { [weak self] items in
+                items.forEach { self?.itemsMap[$0.id] = $0 }
+                self?.itemsInOrder.append(contentsOf: items)
+                self?.fetchedChannelListItems.send(items.map { $0.id })
+            }
+            .store(in: &cancelBag)
+        
+        return fetchedChannelListItems.eraseToAnyPublisher()
+    }
 }
+
