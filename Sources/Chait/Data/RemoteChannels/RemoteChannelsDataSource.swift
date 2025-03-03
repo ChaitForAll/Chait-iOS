96902311fd5eb286b09a4150886c3b740572e68d
yjjem <marksim0606@gmail.com>
2025-03-03 23:37:51 +0900

    feat(RemoteChannels): Add RemoteChannelsDataSourceImplementation

--- Sources/Chait/Data/RemoteChannels/RemoteChannelsDataSource.swift
+++ Sources/Chait/Data/RemoteChannels/RemoteChannelsDataSource.swift
@@ -25,3 +25,77 @@ protocol RemoteChannelsDataSource {
         for memberShip: [ChannelMembershipResponse]
     ) -> AnyPublisher<[ChannelResponse], RemoteChannelsDataSourceError>
 }
+
+final class DefaultRemoteChannelsDataSource: RemoteChannelsDataSource {
+    
+    private let client: SupabaseClient
+    
+    init(client: SupabaseClient) {
+        self.client = client
+    }
+    
+    func fetchChannelMembership(
+        userID: UUID
+    ) -> AnyPublisher<[ChannelMembershipResponse], RemoteChannelsDataSourceError> {
+        return Future { promise in
+            Task {
+                do {
+                    let membershipResponses: [ChannelMembershipResponse] = try await self.client
+                        .from("channel_members")
+                        .select()
+                        .eq("member_id", value: userID)
+                        .execute()
+                        .value
+                    
+                    guard !membershipResponses.isEmpty else {
+                       return promise(.failure(.noItems))
+                    }
+                    
+                    promise(.success(membershipResponses))
+                } catch let urlError as URLError {
+                    promise(.failure(.networkError(urlError)))
+                } catch let postgRestError as PostgrestError {
+                    promise(.failure(.postgRestError(postgRestError)))
+                } catch let httpError as HTTPError {
+                    promise(.failure(.serverError(httpError)))
+                }  catch let decodingError as  DecodingError {
+                    promise(.failure(.decodingError(decodingError)))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func fetchChannels(
+        for memberShip: [ChannelMembershipResponse]
+    ) -> AnyPublisher<[ChannelResponse], RemoteChannelsDataSourceError> {
+        return Future<[ChannelResponse], RemoteChannelsDataSourceError> { promise in
+            Task {
+                do {
+                    let channelResponses: [ChannelResponse] = try await self.client
+                        .from("channel")
+                        .select()
+                        .in("id", values: memberShip.map { $0.channelID })
+                        .execute()
+                        .value
+                    
+                    guard !channelResponses.isEmpty else {
+                       return promise(.failure(.noItems))
+                    }
+                    
+                    promise(.success(channelResponses))
+                    
+                } catch let urlError as URLError {
+                    promise(.failure(.networkError(urlError)))
+                } catch let postgRestError as PostgrestError {
+                    promise(.failure(.postgRestError(postgRestError)))
+                } catch let httpError as HTTPError {
+                    promise(.failure(.serverError(httpError)))
+                }  catch let decodingError as  DecodingError {
+                    promise(.failure(.decodingError(decodingError)))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
