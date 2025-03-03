6a9d0b117da102204c036867bf1b96e34dde41ad
yjjem <marksim0606@gmail.com>
2025-03-03 23:19:01 +0900

    test(Data): Add test_FetchChannelMembershipFailsWithNoChannels

--- Sources/ChaitTests/Unit/Data/ChannelRepositoryTests.swift
+++ Sources/ChaitTests/Unit/Data/ChannelRepositoryTests.swift
@@ -12,4 +12,41 @@ import XCTest
 
 final class ChannelRepositoryTests: XCTestCase {
     
+    private var cancelBag: Set<AnyCancellable> = .init()
+    
+    override func tearDown() {
+        self.cancelBag.removeAll()
+    }
+    
+    func test_FetchChannelMembershipFailsWithNoChannels() {
+        
+        // Arrange
+        
+        let userID = UUID()
+        let expectedMemberships = (0..<5).map { _ in
+            ChannelMembershipResponse(userID: userID, channelID: UUID())
+        }
+        let stubDataSource = StubRemoteChannelDataSourceSucceed(memberShipResponses: expectedMemberships)
+        let sut = DefaultChannelRepository(dataSource: stubDataSource)
+        
+        // Act
+        
+        let receivedErrorExpectation = XCTestExpectation(description: "Received no channels error")
+        sut
+            .fetchChannels(userID: userID)
+            .sink(
+                receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        receivedErrorExpectation.fulfill()
+                        XCTAssertEqual(error, .noChannels)
+                    }
+                },
+                receiveValue: { _ in }
+            )
+            .store(in: &cancelBag)
+        
+        // Assert
+        
+        wait(for: [receivedErrorExpectation], timeout: 1.0)
+    }
 }
