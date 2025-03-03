db51a497211337d85e4fbc2481f6b17ad422e3a1
yjjem <marksim0606@gmail.com>
2025-03-03 12:17:10 +0900

    feat(RemoteChannels): Add RemoteChannelDataSource & RemoteChannelDataSourceError

---
+++ Sources/Chait/Data/RemoteChannels/RemoteChannelsDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  RemoteChannelsDataSource.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+import Combine
+import Supabase
+
+enum RemoteChannelsDataSourceError: Error {
+    case networkError(URLError)
+    case serverError(HTTPError)
+    case postgRestError(PostgrestError)
+    case decodingError(DecodingError)
+}
+
+protocol RemoteChannelsDataSource {
+    func fetchChannelMembership(
+    ) -> AnyPublisher<[ChannelMembershipResponse], RemoteChannelsDataSourceError>
+}
