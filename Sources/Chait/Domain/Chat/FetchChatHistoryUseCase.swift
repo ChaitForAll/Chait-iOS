de90c7062cdf2047973b59882135008ae91df172
yjjem <marksim0606@gmail.com>
2025-03-05 23:34:15 +0900

    feat(Chat): Add FetchChatHistoryUseCase

---
+++ Sources/Chait/Domain/Chat/FetchChatHistoryUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  FetchMessagesUseCase.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+import Combine
+
+enum FetchChatHistoryError: Error {
+    case endOfHistory
+    case networkError
+}
+
+protocol FetchChatHistoryUseCase {
+    func fetchHistory(channelID: UUID, messagesOffset: Int) -> AnyPublisher<[Message], FetchChatHistoryError>
+}
