33d04fa0a84b16cf674c1984f77ebb76b6542d41
yjjem <marksim0606@gmail.com>
2025-03-30 19:08:15 +0900

    feat(Domain): Add Friend entity

---
+++ Sources/Chait/Domain/Friend/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct Friend: Identifiable {
+    let id: UUID
+    let userID: UUID
+    let friendID: UUID
+    let createdAt: Date
+}
