3b8dc4ec290b715b2e3df05e64aaaa07464d3b25
yjjem <marksim0606@gmail.com>
2025-03-31 20:50:07 +0900

    feat(FriendList): Add DiffableDataSource configuration

--- Sources/Chait/Scene/FriendList/FriendListViewController.swift
+++ Sources/Chait/Scene/FriendList/FriendListViewController.swift
@@ -10,7 +10,15 @@ import Combine
 
 final class FriendListViewController: UIViewController {
     
+    // MARK: Type(s)
+    
+    enum Section {
+        case friends
+    }
+    
     // MARK: Property(s)
+    
+    private var diffableDataSource: UICollectionViewDiffableDataSource<Section, UUID>?
     
     private let collectionView: UICollectionView = UICollectionView(
         frame: .zero,
@@ -26,12 +34,20 @@ final class FriendListViewController: UIViewController {
     // MARK: Private Function(s)
     
     private func configureCollectionView() {
+        self.diffableDataSource = createDiffableDataSource()
         collectionView.collectionViewLayout = createListLayout()
+        collectionView.dataSource = diffableDataSource
     }
     
     private func createListLayout() -> UICollectionViewCompositionalLayout {
         let listConfiguration = UICollectionLayoutListConfiguration(appearance: .plain)
         let layout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
         return layout
+    }
+    
+    private func createDiffableDataSource() -> UICollectionViewDiffableDataSource<Section, UUID> {
+        return .init(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            return .init()
+        }
     }
 }
