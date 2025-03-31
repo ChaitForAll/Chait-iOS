4ac84d60d781d1a29755e292bb0583ba0fd11d04
yjjem <marksim0606@gmail.com>
2025-03-31 20:43:41 +0900

    feat(Scene): add fetchFriendsList func to FriendListViewModel

--- Sources/Chait/Scene/FriendListViewModel.swift
+++ Sources/Chait/Scene/FriendListViewModel.swift
@@ -12,11 +12,28 @@ final class FriendListViewModel {
     
     // MARK: Property(s)
     
+    private var cancelBag: Set<AnyCancellable> = .init()
+    private var friendsList: [Friend] = []
+    
     private let userID: UUID
     private let fetchFriendsListUseCase: FetchFriendsListUseCase
     
     init(userID: UUID, fetchFriendsListUseCase: FetchFriendsListUseCase) {
         self.userID = userID
         self.fetchFriendsListUseCase = fetchFriendsListUseCase
+    }
+    
+    // MARK: Private Function(s)
+    
+    private func fetchFriendsList() {
+        fetchFriendsListUseCase
+            .fetchFriendList(userID: userID)
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { friendsList in
+                    self.friendsList.append(contentsOf: friendsList)
+                }
+            )
+            .store(in: &cancelBag)
     }
 }
