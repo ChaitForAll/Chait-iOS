580e182c56d979415d8a5779970d3ceacce35904
yjjem <marksim0606@gmail.com>
2025-04-13 05:52:21 +0900

    feat(Domain): Add User Entity structure

---
+++ Sources/Chait/Domain/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import Foundation
+
+final class User: Identifiable {
+    let id: UUID
+    let userName: String
+    var displayName: String
+    var profileImage: URL
+    let createdAt: Date
+    let updatedAt: Date
+    
+    init(
+        id: UUID,
+        userName: String,
+        displayName: String,
+        profileImage: URL,
+        createdAt: Date,
+        updatedAt: Date
+    ) {
+        self.id = id
+        self.userName = userName
+        self.displayName = displayName
+        self.profileImage = profileImage
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
+}
