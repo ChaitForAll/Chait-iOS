2b7844b972703c3ea0d0b7f6cb51145f15ce1f42
yjjem <marksim0606@gmail.com>
2025-04-13 17:02:08 +0900

    refactor(User): Change User type to interface to support different types of user functionality

--- Sources/Chait/Domain/User.swift
+++ Sources/Chait/Domain/User.swift
@@ -10,47 +10,19 @@ enum UserError: Error {
     case invalidImage
 }
 
-final class User: Identifiable {
-    
-    // MARK: Property(s)
-    
-    let id: UUID
-    let userName: String
-    var displayName: String
-    var profileImage: ProfileImage
-    let createdAt: Date
-    let updatedAt: Date
-    
-    init(
-        id: UUID,
-        userName: String,
-        displayName: String,
-        profileImage: ProfileImage,
-        createdAt: Date,
-        updatedAt: Date
-    ) {
-        self.id = id
-        self.userName = userName
-        self.displayName = displayName
-        self.profileImage = profileImage
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-    }
-    
-    // MARK: Function(s)
-    
+protocol User: AnyObject, Identifiable {
+    var id: UUID { get }
+    var userName: String { get }
+    var displayName: String { get set }
+    var profileImage: ProfileImage? { get set }
+    var createdAt: Date { get }
+}
+
+extension User {
     func changeDisplayName(_ newDisplayName: String) {
         guard !newDisplayName.isEmpty, displayName != newDisplayName else {
             return
         }
         self.displayName = newDisplayName
-    }
-    
-    func changeProfileImage(_ newProfileImage: URL) throws {
-        let newProfileImage = ProfileImage(imageURL: newProfileImage)
-        guard newProfileImage.isValid() else {
-            throw UserError.invalidImage
-        }
-        self.profileImage = newProfileImage
     }
 }
