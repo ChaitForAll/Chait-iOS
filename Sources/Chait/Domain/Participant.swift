fdafc655295901866cd2085682adc4be942644f0
yjjem <marksim0606@gmail.com>
2025-04-13 17:15:24 +0900

    feat(Domain): Add Participant

---
+++ Sources/Chait/Domain/Participant.swift
@@ -0,0 +1,44 @@
+//
+//  Participant.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import Foundation
+
+final class Participant: User {
+    
+    // MARK: Property(s)
+    
+    var displayName: String
+    var profileImage: ProfileImage?
+    
+    let id: UUID
+    let userName: String
+    let createdAt: Date
+    
+    init(
+        id: UUID,
+        userName: String,
+        displayName: String,
+        profileImage: ProfileImage? = nil,
+        createdAt: Date
+    ) {
+        self.id = id
+        self.userName = userName
+        self.displayName = displayName
+        self.profileImage = profileImage
+        self.createdAt = createdAt
+    }
+}
+
+extension Participant: Hashable {
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: Participant, rhs: Participant) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
