c77175ab449ad92cb9b6163d088d763ccb8bd135
yjjem <marksim0606@gmail.com>
2025-04-14 19:20:28 +0900

    feat(Application): Add ConversationRepository Interface

---
+++ Sources/Chait/Application/Interface/ConversationRepository.swift
@@ -0,0 +1,12 @@
+//
+//  ConversationRepository.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import Foundation
+import Combine
+
+protocol ConversationRepository {
+    func fetchConversationList(_ userID: UUID) -> AnyPublisher<[any Conversation], ConversationError>
+}
