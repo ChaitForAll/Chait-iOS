184f0f79205136e172e041af987734c62685a47f
yjjem <marksim0606@gmail.com>
2025-04-14 19:21:25 +0900

    feat(UseCase): Add ConversationUseCase default implementation

--- Sources/Chait/Application/UseCase/ConversationUseCase.swift
+++ Sources/Chait/Application/UseCase/ConversationUseCase.swift
@@ -4,6 +4,7 @@
 //
 //  Copyright (c) 2025 Jeremy All rights reserved.
 
+import Foundation
 import Combine
 
 enum ConversationError: Error {
@@ -12,4 +13,23 @@ enum ConversationError: Error {
 
 protocol ConversationUseCase {
     func fetchConversationList() -> AnyPublisher<[any Conversation], ConversationError>
+}
+
+final class DefaultChatUseCase: ConversationUseCase {
+    
+    // MARK: Property(s)
+    
+    private let conversationRepository: ConversationRepository
+    private let userID: UUID
+    
+    init(conversationRepository: ConversationRepository, userID: UUID) {
+        self.conversationRepository = conversationRepository
+        self.userID = userID
+    }
+    
+    // MARK: Function(s)
+    
+    func fetchConversationList() -> AnyPublisher<[any Conversation], ConversationError> {
+        return conversationRepository.fetchConversationList(userID)
+    }
 }
