064f761ef7596ecadfc7d1bd31c8954e730d4bd4
yjjem <marksim0606@gmail.com>
2025-04-15 10:12:51 +0900

    feat(NetworkStack): Add ConversationMembershipRemoteDataSource implementation

--- Sources/Chait/Data/NetworkStack/ConversationMembershipRemoteDataSource.swift
+++ Sources/Chait/Data/NetworkStack/ConversationMembershipRemoteDataSource.swift
@@ -5,7 +5,32 @@
 //  Copyright (c) 2025 Jeremy All rights reserved.
     
 import Foundation
+import Supabase
 
 protocol ConversationMembershipRemoteDataSource {
     func fetchConversationMemberships(_ userID: UUID) async throws -> [ConversationMembershipResponse]
+}
+
+final class DefaultConversationMembershipRemoteDataSource: ConversationMembershipRemoteDataSource {
+    
+    // MARK: Property(s)
+    
+    private let supabase: SupabaseClient
+    
+    init(supabase: SupabaseClient) {
+        self.supabase = supabase
+    }
+    
+    // MARK: Function(s)
+    
+    func fetchConversationMemberships(
+        _ userID: UUID
+    ) async throws -> [ConversationMembershipResponse] {
+        return try await supabase
+            .from("conversation_membership")
+            .select()
+            .eq("userID", value: userID)
+            .execute()
+            .value
+    }
 }
