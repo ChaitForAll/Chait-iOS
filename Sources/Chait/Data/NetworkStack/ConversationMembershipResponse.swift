148ae1f557088763f0cc26dfd7dc8625246cf651
yjjem <marksim0606@gmail.com>
2025-04-15 10:09:22 +0900

    feat(NetworkStack): Add ConversationMembershipResponse

---
+++ Sources/Chait/Data/NetworkStack/ConversationMembershipResponse.swift
@@ -0,0 +1,14 @@
+//
+//  ConversationMembershipResponse.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+
+import Foundation
+    
+struct ConversationMembershipResponse: Decodable {
+    let id: UUID
+    let conversationID: UUID
+    let createdAt: UUID
+}
+
