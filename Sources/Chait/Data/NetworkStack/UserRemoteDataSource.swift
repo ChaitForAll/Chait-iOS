16853ffcee832e558635a6cef6b7e5418adf1284
yjjem <marksim0606@gmail.com>
2025-04-15 21:35:40 +0900

    feat(UserRemoveDataSource): Add UserRemoteDataSource default implementation

--- Sources/Chait/Data/NetworkStack/UserRemoteDataSource.swift
+++ Sources/Chait/Data/NetworkStack/UserRemoteDataSource.swift
@@ -5,8 +5,40 @@
 //  Copyright (c) 2025 Jeremy All rights reserved.
 
 import Foundation
+import Supabase
 
 protocol UserRemoteDataSource {
     func fetchUser(_ userIdentifier: UUID) async throws -> UserResponse
     func fetchUsers(_ userIdentifiers: [UUID]) async throws -> [UserResponse]
+}
+
+final class DefaultUserRemoteDataSource: UserRemoteDataSource {
+    
+    // MARK: Property(s)
+    
+    private let supabase: SupabaseClient
+    
+    init(supabase: SupabaseClient) {
+        self.supabase = supabase
+    }
+    
+    // MARK: Function(s)
+    
+    func fetchUser(_ userIdentifier: UUID) async throws -> UserResponse {
+        return try await supabase
+            .from("user_profile")
+            .select()
+            .eq("id", value: userIdentifier)
+            .execute()
+            .decode(using: .convertFromSnakeCase)
+    }
+    
+    func fetchUsers(_ userIdentifiers: [UUID]) async throws -> [UserResponse] {
+        return try await supabase
+            .from("user_profile")
+            .select()
+            .in("id", values: userIdentifiers)
+            .execute()
+            .decode(using: .convertFromSnakeCase)
+    }
 }
