aceeb03a0801b60a0994b9712872cad8381dc708
yjjem <marksim0606@gmail.com>
2025-04-17 03:21:18 +0900

    feat(Domain): Add ConversationSummary

---
+++ Sources/Chait/Domain/ConversationSummary.swift
@@ -0,0 +1,12 @@
+//
+//  ConversationSummary.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import Foundation
+
+struct ConversationSummary: Identifiable {
+    let id: UUID
+    let title: String
+}
