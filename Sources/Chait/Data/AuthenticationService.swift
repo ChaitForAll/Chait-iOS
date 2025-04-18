d39abe52e68bcfe6dbfb82710b61563a2db0116d
yjjem <marksim0606@gmail.com>
2025-04-19 02:11:18 +0900

    feat(Data): Add AuthenticationService

---
+++ Sources/Chait/Data/AuthenticationService.swift
@@ -0,0 +1,33 @@
+//
+//  AuthenticationService.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+
+import Foundation
+import Supabase
+
+    
+final class AuthenticationService {
+    
+    // MARK: Property(s)
+    
+    var userID: UUID?
+    
+    private let supabase: SupabaseClient
+    
+    init(supabase: SupabaseClient) {
+        self.supabase = supabase
+    }
+    
+    // MARK: Function(s)
+    
+    func signIn(_ email: String, password: String) async throws {
+        let auth = try await supabase.auth.signIn(email: email, password: password)
+        self.userID = auth.user.id
+    }
+    
+    func logOut() async throws {
+        try await supabase.auth.signOut()
+    }
+}
