03f05754258a27437e07333ee15d7e10b47797f2
yjjem <marksim0606@gmail.com>
2025-04-27 13:24:53 +0900

    feat(ConversationRemote): Add message insertion

--- Sources/Chait/Data/NetworkStack/DataSource/ConversationRemoteDataSource.swift
+++ Sources/Chait/Data/NetworkStack/DataSource/ConversationRemoteDataSource.swift
@@ -12,6 +12,7 @@ protocol ConversationRemoteDataSource {
     func fetchConversations(
         _ conversationIdentifiers: [UUID]
     ) async throws -> [ConversationResponse]
+    func insertNewMessage(_ request: NewMessageRequest) async throws
 }
 
 final class DefaultConversationRemoteDataSource: ConversationRemoteDataSource {
@@ -35,5 +36,12 @@ final class DefaultConversationRemoteDataSource: ConversationRemoteDataSource {
             .in("id", values: conversationIdentifiers)
             .execute()
             .decodeJSON()
+    }
+    
+    func insertNewMessage(_ request: NewMessageRequest) async throws {
+        try await supabase
+            .from("messages")
+            .insert(request)
+            .execute()
     }
 }
