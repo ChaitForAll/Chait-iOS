56e701f75e1f8870f2ba388e96c37eda47dd87e4
yjjem <marksim0606@gmail.com>
2025-04-27 12:44:47 +0900

    feat(Domain): Add NewMessage entity

---
+++ Sources/Chait/Domain/NewMessage.swift
@@ -0,0 +1,13 @@
+//
+//  NewMessage.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import Foundation
+
+struct NewMessage {
+    let text: String
+    let senderID: UUID
+    let conversationID: UUID
+}
