16e2804e0773f9b888c1ca0f7d165034602e42a2
yjjem <marksim0606@gmail.com>
2025-05-04 16:28:51 +0900

    feat(Scene): Add MonoSecondarBorderedButton

---
+++ Sources/Chait/Scene/CommonDesign/MonoSecondaryBorderedButton.swift
@@ -0,0 +1,27 @@
+//
+//  MonoSecondaryBorderedButton.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import UIKit
+
+final class MonoSecondaryBorderedButton: UIButton {
+    
+    // MARK: Override(s)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        var alreadyHaveAccountConfiguration = UIButton.Configuration.bordered()
+        alreadyHaveAccountConfiguration.baseBackgroundColor = .systemGray6
+        alreadyHaveAccountConfiguration.baseForegroundColor = .label
+        alreadyHaveAccountConfiguration.background.strokeColor = .lightGray
+        self.configuration = alreadyHaveAccountConfiguration
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
