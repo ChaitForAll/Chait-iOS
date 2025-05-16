ddfc86a7be8759df3b1384fa0edbd2990c9a46a0
yjjem <marksim0606@gmail.com>
2025-05-17 02:40:02 +0900

    feat(Scene): Add UICollectionViewListCell+Configuration

---
+++ Sources/Chait/Scene/Extension/UICollectionViewListCell+Configuration.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewListCell.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+
+import UIKit
+
+extension UICollectionViewListCell {
+    
+    func friedListContentConfiguration() -> FriendListCellContentView.Configuration {
+        return FriendListCellContentView.Configuration()
+    }
+}
