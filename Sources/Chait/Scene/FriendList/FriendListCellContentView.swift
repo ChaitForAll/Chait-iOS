1b6bc44182f32bbc036d6610fe74a1bdd49530a2
yjjem <marksim0606@gmail.com>
2025-05-17 02:39:43 +0900

    feat(FriendList): Add FriendListContentView implementation + Configuration

--- Sources/Chait/Scene/FriendList/FriendListCellContentView.swift
+++ Sources/Chait/Scene/FriendList/FriendListCellContentView.swift
@@ -7,21 +7,94 @@
 import UIKit
 
 final class FriendListCellContentView: UIView,  UIContentView {
-
+    struct Configuration: UIContentConfiguration {
+        var image: UIImage?
+        var name: String?
+        var status: String?
+        var imageToTextSpacing: CGFloat = 8
+        var imageWidth: CGFloat = 55
+        var contentInset: UIEdgeInsets = .init(top: 2, left: 16, bottom: 2, right: 16)
+        var separatorInset: CGFloat {
+            return contentInset.left + imageToTextSpacing + imageWidth
+        }
+        
+        func makeContentView() -> any UIView & UIContentView {
+            FriendListCellContentView(configuration: self)
+        }
+        
+        func updated(for state: any UIConfigurationState) -> Self {
+            return self
+        }
+    }
+    
     // MARK: Property(s)
     
-    var configuration: any UIContentConfiguration
+    private let contentStack = UIStackView()
+    private let imageView = UIImageView()
+    private let userInfoStack = UIStackView()
+    private let nameLabel = UILabel()
+    private let statusLabel = UILabel()
     
-    // MARK: Initializer(s)
+    private var imageWidthConstraint: NSLayoutConstraint?
+    
+    // MARK: Override(s) & Requirement(s)
+    
+    var configuration: any UIContentConfiguration {
+        didSet {
+            apply(configuration: configuration)
+        }
+    }
     
     init(configuration: UIContentConfiguration) {
         self.configuration = configuration
         super.init(frame: .zero)
+        configureLayoutHierarchy()
+        configureLayoutStyle()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: Function(s)
+    // MARK: Private Function(s)
+    
+    private func configureLayoutHierarchy() {
+        addSubview(contentStack)
+        contentStack.translatesAutoresizingMaskIntoConstraints = false
+        contentStack.addArrangedSubview(imageView)
+        contentStack.addArrangedSubview(userInfoStack)
+        imageWidthConstraint = imageView.widthAnchor.constraint(equalToConstant: 55)
+        imageWidthConstraint?.isActive = true
+        NSLayoutConstraint.activate([
+            contentStack.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            contentStack.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            contentStack.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            contentStack.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        ])
+        userInfoStack.addArrangedSubview(nameLabel)
+        userInfoStack.addArrangedSubview(statusLabel)
+    }
+    
+    private func configureLayoutStyle() {
+        userInfoStack.axis = .vertical
+        userInfoStack.distribution = .fillEqually
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 0.5 * (55)
+        imageView.clipsToBounds = true
+        nameLabel.font = .preferredFont(forTextStyle: .headline)
+        statusLabel.font = .preferredFont(forTextStyle: .subheadline)
+    }
+    
+    private func apply(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else {
+            return
+        }
+        imageView.image = configuration.image
+        nameLabel.text = configuration.name
+        statusLabel.text = configuration.status
+        layoutMargins = configuration.contentInset
+        contentStack.setCustomSpacing(configuration.imageToTextSpacing, after: imageView)
+        imageWidthConstraint?.constant = configuration.imageWidth
+    }
 }
