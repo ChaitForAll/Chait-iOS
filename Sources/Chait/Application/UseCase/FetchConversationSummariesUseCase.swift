4cf63814384fc0d3d573fd19dec8b34ca997fbc1
yjjem <marksim0606@gmail.com>
2025-05-21 00:57:35 +0900

    feat(UseCase): Implement FetchConversationSummariesUseCase

--- Sources/Chait/Application/UseCase/FetchConversationSummariesUseCase.swift
+++ Sources/Chait/Application/UseCase/FetchConversationSummariesUseCase.swift
@@ -16,9 +16,17 @@ final class FetchConversationSummariesUseCase {
         case unknown
     }
     
+    // MARK: Property(s)
+    
+    private let conversationRepository: ConversationRepository
+    
+    init(conversationRepository: ConversationRepository) {
+        self.conversationRepository = conversationRepository
+    }
+    
     // MARK: Function(s)
     
     func execute() -> AnyPublisher<[ConversationSummary], ExecutionError> {
-        return Empty().eraseToAnyPublisher()
+        return conversationRepository.fetchConversationSummaryList()
     }
 }
