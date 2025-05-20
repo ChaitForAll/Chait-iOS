dd167622ded7e47e8e30fb0265dd2695e6b69e6a
yjjem <marksim0606@gmail.com>
2025-05-20 22:58:02 +0900

    feat(Domain): Add titleImage, lasMessage variables mapping from conversationResponse
    mapping was necessary due to use in usecase taht would be deprecated in the future

--- Sources/Chait/Domain/ConversationSummary.swift
+++ Sources/Chait/Domain/ConversationSummary.swift
@@ -9,4 +9,13 @@ import Foundation
 struct ConversationSummary: Identifiable {
     let id: UUID
     let title: String
+    let titleImage: String?
+    var lastMessage: Message?
+    
+    init(_ conversationResponse: ConversationResponse) {
+        self.id = conversationResponse.id
+        self.title = conversationResponse.title
+        self.titleImage = nil
+        self.lastMessage = nil
+    }
 }
