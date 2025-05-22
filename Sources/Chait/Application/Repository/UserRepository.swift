bed04f3e5976b25b69e6695084934255eb4b4a9b
yjjem <marksim0606@gmail.com>
2025-05-22 20:29:56 +0900

    feat(Repository): Add UserRepository fetchUserDetails requirement

---
+++ Sources/Chait/Application/Repository/UserRepository.swift
@@ -0,0 +1,19 @@
+//
+//  UserRepository.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+import Combine
+
+enum UserRepositoryError: Error {
+    case unknown
+}
+
+protocol UserRepository {
+    func fetchUserDetails(
+        _ userIdentifiers: [UUID]
+    ) -> AnyPublisher<[UserDetail], UserRepositoryError>
+}
