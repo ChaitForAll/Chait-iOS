918c678ac2639f7c4119f0246b9711689aa8b696
yjjem <marksim0606@gmail.com>
2025-05-24 17:57:00 +0900

    feat(SendMessage): Implement SendMessageUseCase

--- Sources/Chait/Application/UseCase/SendMessage/SendMessageUseCase.swift
+++ Sources/Chait/Application/UseCase/SendMessage/SendMessageUseCase.swift
@@ -14,3 +14,26 @@ enum SendMessageError: Error {
 protocol SendMessageUseCaseInterface {
     func execute(_ command: SendMessageCommand) async -> Result<Message, SendMessageError>
 }
+
+final class SendMessageUseCase: SendMessageUseCaseInterface {
+    
+    // MARK: Property(s)
+    
+    private let messageRepository: MessageRepository
+    
+    init(messageRepository: MessageRepository) {
+        self.messageRepository = messageRepository
+    }
+    
+    // MARK: Function(s)
+    
+    func execute(_ command: SendMessageCommand) async -> Result<Message, SendMessageError> {
+        let newMessage = NewMessage(text: command.message, conversationID: command.conversationIdentifier)
+        let sendingResult = await messageRepository.create(newMessage)
+        return sendingResult.mapError { repositoryError in
+            switch repositoryError {
+            case .unknown: .unknown
+            }
+        }
+    }
+}
