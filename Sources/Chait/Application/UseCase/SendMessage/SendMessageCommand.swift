04d5a86eebb0896adf14ddf60087b8a8c1945974
yjjem <marksim0606@gmail.com>
2025-05-24 16:55:04 +0900

    feat(UseCase): Add SendMessageCommand

---
+++ Sources/Chait/Application/UseCase/SendMessage/SendMessageCommand.swift
@@ -0,0 +1,12 @@
+//
+//  SendMessageCommand.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+import Foundation
+
+struct SendMessageCommand {
+    let message: String
+    let conversationIdentifier: UUID
+}
