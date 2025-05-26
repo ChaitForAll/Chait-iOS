ca8ebce78a2c2c77543b8f146cf0d55eeacb43ac
yjjem <marksim0606@gmail.com>
2025-05-26 22:58:42 +0900

    feat(FetchConversationHistory): Add conversation identifier variable

--- Sources/Chait/Application/UseCase/FetchConversationHistory/FetchConversationHistoryCommand.swift
+++ Sources/Chait/Application/UseCase/FetchConversationHistory/FetchConversationHistoryCommand.swift
@@ -5,12 +5,15 @@
 //  Copyright (c) 2025 Jeremy All rights reserved.
     
 
+import Foundation
+
 enum MessageQuery {
     case mostRecent
     case before(Message)
 }
 
 struct FetchConversationHistoryCommand {
+    let conversationIdentifier: UUID
     let query: MessageQuery
     let limit: Int
 }
