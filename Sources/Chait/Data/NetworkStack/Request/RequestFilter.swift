c5eca9abe300f94fe7bb9b9a5ce2161c63292d77
yjjem <marksim0606@gmail.com>
2025-05-26 22:52:45 +0900

    feat(NetworkStack): Add RequestFilter

---
+++ Sources/Chait/Data/NetworkStack/Request/RequestFilter.swift
@@ -0,0 +1,12 @@
+//
+//  RequestFilter.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+
+
+struct RequestFilter {
+    let column: String
+    let requestOperator: String
+    let value: String
+}
