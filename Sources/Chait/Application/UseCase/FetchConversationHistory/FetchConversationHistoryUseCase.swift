7cf3ca67f4a4cc54cfd72a6379c9ddd170dface4
yjjem <marksim0606@gmail.com>
2025-05-26 22:59:09 +0900

    feat(FetchConversationHistory): Implement FetchConversationHistoryUseCase

--- Sources/Chait/Application/UseCase/FetchConversationHistory/FetchConversationHistoryUseCase.swift
+++ Sources/Chait/Application/UseCase/FetchConversationHistory/FetchConversationHistoryUseCase.swift
@@ -16,3 +16,33 @@ protocol FetchConversationHistoryUseCase {
 }
 
 
+final class DefaultFetchConversationHistory: FetchConversationHistoryUseCase {
+    
+    // MARK: Property(s)
+    
+    private let repository: MessageRepository
+    
+    init(repository: MessageRepository) {
+        self.repository = repository
+    }
+    
+    // MARK: Function(s)
+    
+    func execute(
+        _ command: FetchConversationHistoryCommand
+    ) async -> Result<[ConversationHistoryItem], FetchConversationHistoryError> {
+        await repository.fetchMessages(
+            from: command.conversationIdentifier,
+            query: command.query,
+            limit: command.limit
+        )
+        .mapError { repositoryError in
+            switch repositoryError {
+            case .unknown: FetchConversationHistoryError.unknown
+            }
+        }
+        .map { messages in
+            messages.map { .message($0) }
+        }
+    }
+}
