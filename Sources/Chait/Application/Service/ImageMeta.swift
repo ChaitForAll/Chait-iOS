1952fbc3588d69a8e3298dccecc8a6a0cfe9743d
yjjem <marksim0606@gmail.com>
2025-06-17 21:41:25 +0900

    feat(Domain): Add ImageMeta

---
+++ Sources/Chait/Application/Service/ImageMeta.swift
@@ -0,0 +1,14 @@
+//
+//  ImageMeta.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct ImageMeta {
+    let sourceURL: URL
+    let createdAt: Date
+    let updatedAt: Date
+}
