01d279b81c1984b32fd9b40800625c14719c6548
yjjem <marksim0606@gmail.com>
2025-06-21 14:39:58 +0900

    feat(Service): Add UserImage & UserImageType

---
+++ Sources/Chait/Application/Service/UserImage.swift
@@ -0,0 +1,13 @@
+//
+//  UserImage.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct UserImage {
+    let data: Data
+    let imageType: UserImageType
+}
---
+++ Sources/Chait/Application/Service/UserImageType.swift
@@ -0,0 +1,10 @@
+//
+//  UserImageType.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+
+
+enum UserImageType {
+    case profile
+}
