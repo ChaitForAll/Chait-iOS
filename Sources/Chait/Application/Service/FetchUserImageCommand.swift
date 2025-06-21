d7fc2e697c7803a80abc932020378d54a9e3636a
yjjem <marksim0606@gmail.com>
2025-06-21 14:40:23 +0900

    feat(Service): Add FetchUserImageCommand

---
+++ Sources/Chait/Application/Service/FetchUserImageCommand.swift
@@ -0,0 +1,13 @@
+//
+//  FetchUserImageCommand.swift
+//  Chait
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct FetchUserImageCommand {
+    let userID: UUID
+    let imageType: UserImageType
+}
